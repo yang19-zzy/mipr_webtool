ac58fbfd72fe1e66d458448a0a75b8fc60ad215f
Zhengyang Zhao <zzyang@umich.edu>
2025-02-27 15:02:31 -0500

    debug

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ st.title("Database Access Demo")
 
 st.write(st.query_params)
 
+state = None
+code = None
+scope = None
+authuser = None
+hd = None
+prompt = None
+user_id, user_email = None, None
+client = OAuth2Session(
+    client_id=st.secrets['auth']['client_id'],
+    client_secret=st.secrets['auth']['client_secret'],
+    scope=st.secrets['auth']['scope'],
+    redirect_uri=st.secrets['auth']['redirect_uri'],
+    token_endpoint_auth_method='client_secret_post'
+)
+
 
 pages = [
     st.Page("p0_home.py", title='home', icon="🏠"), 
@@ -27,23 +42,50 @@ pg = st.navigation(pages)
 pg.run()
 
 st.write(st.session_state)
-st.write(st.experimental_user)
+# st.write(st.experimental_user)
 # st.write(UserInfo.values())
 
-if not st.experimental_user.is_logged_in:
+try:
+    st.query_params['code']
+except KeyError:
+    st.query_params['code'] = None
+
+if not st.query_params['code']:
     with st.container():
         if st.button("Log in"):
             st.login()
             # st.write(st.experimental_user)
 
             #add user login record to database
+             
 else:
     #if logged in, display content
+    
+
+    state = st.query_params['state']
+    code = st.query_params['code']
+    scope = st.query_params['scope']
+    authuser = st.query_params['authuser']
+    hd = st.query_params['hd']
+    prompt = st.query_params['prompt'] 
+
+    client = OAuth2Session(
+        client_id=st.secrets['auth']['client_id'],
+        client_secret=st.secrets['auth']['client_secret'],
+        scope=st.secrets['auth']['scope'],
+        redirect_uri=st.secrets['auth']['redirect_uri'],
+        token_endpoint_auth_method='client_secret_post'
+    )
+    user_id, user_email = client.get_id_email(code)
+    st.experimental_user.email = user_email
+    st.experimental_user.name = user_id
+
+
     st.write(f"Logged in as {st.experimental_user.email}")
     st.write(f"User ID: {st.experimental_user.name}")
 
-    st.write(st.query_params['code'])
 
+    
     with st.sidebar:
         st.title("Navigation")
         logout = st.button("Log out")
